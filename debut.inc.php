e6eaa9d05a4ead231308847afe3d0ab981678b45
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-06-08 18:27:24 +0200

    Auto Commit

---
+++ debut.inc.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="icon" type="image/jpg" sizes="16x16" href="assets/LOGO.jpg">
