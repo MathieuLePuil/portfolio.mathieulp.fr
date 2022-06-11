b302460d79fed16889d267396a7fc6159a8a9274
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-06-11 12:34:24 +0200

    Auto Commit

--- header.inc.php
+++ header.inc.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <div class="logo">
-            <img src="assets/LOGO.png" alt="LOGO" id="logohg" width="27" height="27">
+            <img src="assets/LOGO.png" alt="LOGO" id="logohg" width="35" height="35">
             <h1 class="logotxt no-background-txt-black">PORT</h1>
             <h1 class="background-txt">FOLIO</h1>
         </div>
