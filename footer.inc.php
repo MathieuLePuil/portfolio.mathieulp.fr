c543160b4dcf463ed20ad2d61c9b98eeea34c79f
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-07-05 11:46:54 +0200

    Footer html (no css)

---
+++ footer.inc.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="reseaux">
+        <h2>ME SUIVRE</h2>
+        <a href=""><img src="assets/GITHUB-small.png"> GitHub</a>
+        <a href=""><img src="assets/LINKEDIN-small.png"> Linkedin</a>
+    </div>
+    <div class="navigation">
+        <h2>NAVIGATION</h2>
+        <a href="#home">Accueil</a>
+        <a href="#about">À propos</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+    </div>
+    <div class="coordonnees">
+        <h2>COORDONNEES</h2>
+        <a href=""><img src="assets/email.png"> contact@mathieulp.fr</a>
+        <a href=""><img src="assets/phone.png"> 07.82.91.70.75</a>
+    </div>
+</footer>
