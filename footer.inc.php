43b0c47b52e929a913adc8846effa76e06f8c075
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-07-05 11:58:04 +0200

    Footer terminé

--- footer.inc.php
+++ footer.inc.php
@@ -1,19 +1,31 @@
 <footer>
     <div class="reseaux">
-        <h2>ME SUIVRE</h2>
-        <a href=""><img src="assets/GITHUB-small.png"> GitHub</a>
-        <a href=""><img src="assets/LINKEDIN-small.png"> Linkedin</a>
+        <div>
+            <h2>ME SUIVRE</h2>
+        </div>
+        <div class="ft-link">
+            <a href=""><img src="assets/GITHUB-small.png"> GitHub</a>
+            <a href=""><img src="assets/LINKEDIN-small.png"> Linkedin</a>
+        </div>
     </div>
     <div class="navigation">
-        <h2>NAVIGATION</h2>
-        <a href="#home">Accueil</a>
-        <a href="#about">À propos</a>
-        <a href="#portfolio">Portfolio</a>
-        <a href="#contact">Contact</a>
+        <div>
+            <h2>NAVIGATION</h2>
+        </div>
+        <div class="ft-link">
+            <a href="#home">Accueil</a>
+            <a href="#about">À propos</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#contact">Contact</a>
+        </div>
     </div>
     <div class="coordonnees">
-        <h2>COORDONNEES</h2>
-        <a href=""><img src="assets/email.png"> contact@mathieulp.fr</a>
-        <a href=""><img src="assets/phone.png"> 07.82.91.70.75</a>
+        <div>
+            <h2>COORDONNEES</h2>
+        </div>
+        <div class="ft-link">
+            <a href=""><img src="assets/email.png"> contact@mathieulp.fr</a>
+            <a href=""><img src="assets/phone.png"> 07.82.91.70.75</a>
+        </div>
     </div>
 </footer>
