03d20b61b1a4730f6ac1d633db4b0ca5621bd8af
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-04-26 08:57:23 +0200

    :globe_with_meridians: | Import des scripts dans fin.inc.php

--- debut.inc.php
+++ debut.inc.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="style/style.css">
     <link rel="icon" type="image/jpg" sizes="16x16" href="assets/LOGO.png">
     <title>Portfolio - Mathieu Le Puil</title>
-    <script src="https://kit.fontawesome.com/f89af89e77.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
--- fin.inc.php
+++ fin.inc.php
@@ -1,2 +1,4 @@
-</body>
+        <script src="script/animation.js"></script>
+        <script src="https://kit.fontawesome.com/f89af89e77.js" crossorigin="anonymous"></script>
+    </body>
 </html>
